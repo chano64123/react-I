e1a8d44aecead07a306ce3275958a151f77c78a3
chano64123 <promo201674@gmail.com>
2024-05-29 16:40:10 -0500

    dynamic product render

--- src/views/Details/index.tsx
+++ src/views/Details/index.tsx
@@ -5,36 +5,43 @@ import ProductList from '@/components/ProductList';
 import ProductDetailImage from '@/components/ProductDetailImage';
 import ProductDetailDescription from '@/components/ProductDetailDescription';
 import ProductDetailCheckout from '@/components/ProductDetailCheckout';
+import Hero from '@/components/Hero';
 import data from '@/data/products.json'
+import { useParams } from 'react-router-dom';
 
 const Details = () => {
-    const product = data.products[0];
+    const { id } = useParams();
+    const product = data.products.find(product => product.id === Number(id));
     return (
         <>
             <Header />
-            <main>
-                <div className="container">
-                    <div className="columns-container">
-                        <div className="product-images-block" id="product-images">
-                            <ProductDetailImage product={product} />
-                        </div>
-                        <div className="product-description-block" id="product-description">
-                            <ProductDetailDescription product={product} />
-                        </div>
-                        <div className="product-checkout-block" id="product-checkout">
-                            <ProductDetailCheckout product={product}/>
-                        </div>
-                    </div>
-                    <div className="sales-block">
-                        <h3 className="recommended-product-label">Productos que regalamos</h3>
+            {
+                !product ? <Hero first={"404"} second={"Producto no encontrado"} /> :
+                    <main>
                         <div className="container">
-                            <div className="product-container" id="offer-products">
-                                <ProductList first={3} />
+                            <div className="columns-container">
+                                <div className="product-images-block" id="product-images">
+                                    <ProductDetailImage product={product} />
+                                </div>
+                                <div className="product-description-block" id="product-description">
+                                    <ProductDetailDescription product={product} />
+                                </div>
+                                <div className="product-checkout-block" id="product-checkout">
+                                    <ProductDetailCheckout product={product} />
+                                </div>
+                            </div>
+                            <div className="sales-block">
+                                <h3 className="recommended-product-label">Productos que regalamos</h3>
+                                <div className="container">
+                                    <div className="product-container" id="offer-products">
+                                        <ProductList first={3} />
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </main>
+                    </main>
+
+            }
             <Footer />
         </>
     );
