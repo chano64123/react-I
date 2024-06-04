b9739b28919325e315ca3bfbf83bdef22c677545
chano64123 <promo201674@gmail.com>
2024-06-04 14:55:20 -0500

    useState on Thumb

--- src/components/DetailThumbs/index.tsx
+++ src/components/DetailThumbs/index.tsx
@@ -1,16 +1,32 @@
 import './styles.css'
+import defaultThumb from '/public/images/products/mock1.jpg'
+import { useEffect, useState } from 'react';
 
 const DetailThumbs = ({ product }) => {
+    const [thumb, setThumb] = useState(product.images[0] || defaultThumb);
+    useEffect(() => {
+        setThumb(product.images[0] || defaultThumb);
+        console.log("cambio de imagen");
+    }, [product])
     return (
         <>
             {
                 product.images.map((image, index) => (
-                    <div key={index} className="thumbnail-container"><img src={image} alt={`miniatura ${index + 1}`} /></div>
+                    <div key={index} className="thumbnail-container">
+                        <img
+                            src={image}
+                            alt={`miniatura ${product.title}`}
+                            onClick={() => setThumb(image)}
+                        />
+                    </div>
                 ))
             }
-            <img id="main-image" className="main-image" src={product.images[0]} alt="imagen principal" />
+            <img
+                className="main-image"
+                src={thumb}
+                alt={product.tutle}
+            />
         </>
-
     );
 }
 
